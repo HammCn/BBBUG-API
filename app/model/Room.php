4099ef95e1d5c8e40d8b28d6be3de311bd48eaea
Hamm <majhamm@qq.com>
2020-12-10 15:27:25 +0800

    rm: 移出777房间默认显示

--- app/model/Room.php
+++ app/model/Room.php
@@ -10,7 +10,7 @@ class Room extends BaseModel
     {
         $resource = $this->view('room', $field)->view('user', 'user_id,user_name,user_group,user_head', 'room.room_user = user.user_id');
         
-        $resource = $resource->whereRaw("(room_online > 0 or room_id = 888 or room_id = 777) and room_hide = 0");
+        $resource = $resource->whereRaw("(room_online > 0 or room_id = 888) and room_hide = 0");
         if ($order) {
             $resource = $resource->order($order);
         }
