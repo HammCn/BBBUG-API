c1076e8219561ca967fbe4bb443328b1f75480a2
Hamm <majhamm@qq.com>
2021-02-27 12:31:20 +0800

    add: 羊头详情接口

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -16,4 +16,11 @@ class Index extends BaseController
             'data'=>json_decode(file_get_contents('https://h5.oschina.net/apiv3/projectRecommend?size=50&page=1'),true)['data']['items']
         ]);
     }
+    public function detail(){
+        if(!input('id')){
+            return jerr('参数错误');
+        }
+        $id = intval(input('id'));
+        return jok("Hello World!",json_decode(file_get_contents('https://h5.oschina.net/apiv3/projectDetail?id='.$id),true)['data']['project']);
+    }
 }
