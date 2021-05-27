1c060a9f31d0e0914fd7a3fa5b5ded2f078871e4
Hamm <majhamm@qq.com>
2021-05-27 17:11:43 +0800

    add: 封装第三方调用SDK

---
+++ public/sdk/bbbug.js
@@ -0,0 +1,55 @@
+var BBBUG = {
+    sendTextMessage(message) {
+        parent.window.postMessage({
+            event: "sendTextMessage",
+            message: message
+        }, "*");
+    },
+    hideLrc() {
+        parent.window.postMessage({
+            event: "hideLrc"
+        }, "*");
+    },
+    showLrc() {
+        parent.window.postMessage({
+            event: "showLrc"
+        }, "*");
+    },
+    setVolume(volume) {
+        parent.window.postMessage({
+            event: "setVolume",
+            volume: volume
+        }, "*");
+    },
+    showUserInfo(userId) {
+        parent.window.postMessage({
+            event: "showUserInfo",
+            userId: userId
+        }, "*");
+    },
+    showPlayer() {
+        parent.window.postMessage({
+            event: "showPlayer"
+        }, "*");
+    },
+    hidePlayer() {
+        parent.window.postMessage({
+            event: "hidePlayer"
+        }, "*");
+    },
+    passTheSong() {
+        parent.window.postMessage({
+            event: "passTheSong"
+        }, "*");
+    },
+    loveTheSong() {
+        parent.window.postMessage({
+            event: "loveTheSong"
+        }, "*");
+    },
+    getNowSong() {
+        parent.window.postMessage({
+            event: "getNowSong"
+        }, "*");
+    }
+};
